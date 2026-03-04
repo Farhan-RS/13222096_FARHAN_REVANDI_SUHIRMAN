d2a379cac29c6904854a1615dbe31c94f7031cf0
Farhan-RS <130888634+Farhan-RS@users.noreply.github.com>
2026-03-04 12:56:34 +0700

    Update latihan_reduction.cpp

--- praktikum_9/latihan_reduction.cpp
+++ praktikum_9/latihan_reduction.cpp
@@ -3,7 +3,7 @@
 
 int main() {  
     std::vector<int> data = {10, 20, 30, 40};  
-   // int total = 0; // <-- TARGET BENTROKAN (DATA RACE)
+    int total = 0; // <-- TARGET BENTROKAN (DATA RACE)
 
     // Simulasi: Jika 4 Core berebut mengubah variabel 'total' bersamaan  
     for(int i = 0; i < data.size(); i++) {  
